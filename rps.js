09a2b0408c7a3d6c43488cbe385558f60a14cf0c
Joel <joel.v.paul@outlook.com>
2022-09-20 19:27:44 +1000

    Add display for round and score

--- rps.js
+++ rps.js
@@ -6,15 +6,33 @@ const WIN = 1;
 const LOSE = -1;
 const DRAW = 0;
 
-const ROUNDS = 5;
+const MAX_ROUNDS = 5;
+
+let round = 0;
+let wins = 0;
+let losses = 0;
+
+const outcomeDiv = document.querySelector('.outcome');
+const roundDiv = document.querySelector('.round');
+const winsDiv = document.querySelector('.wins');
+const lossesDiv = document.querySelector('.losses');
+const drawsDiv = document.querySelector('.draws');
 
 const buttonOptions = document.querySelectorAll('.option');
 buttonOptions.forEach(option => {
   option.addEventListener('click', function(e) {
-    const round = playRound(e.target.innerText, getComputerChoice());
-    alert(round);
+    outcomeDiv.innerText = playRound(e.target.innerText, getComputerChoice());
+    updateText();
   });
 });
+
+function updateText() {
+  roundDiv.innerText = "Round: " + round;
+  winsDiv.innerText = "Wins: " + wins;
+  lossesDiv.innerText = "Losses: " + losses;
+  const draws = round - wins - losses;
+  drawsDiv.innerText = "Draws: " + draws;
+}
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
@@ -23,12 +41,15 @@ function playRound(playerSelection, computerSelection) {
   if (!OPTIONS.includes(playerSelection) || !OPTIONS.includes(computerSelection)) {
     return "Invalid Input";
   }
+  round++;
 
   const outcome = getOutcome(playerSelection, computerSelection);
   if (outcome === WIN) {
+    wins++;
     return "You Win! " + playerSelection + " beats " + computerSelection;
   }
   if (outcome === LOSE) {
+    losses++;
     return "You Lose! " + computerSelection + " beats " + playerSelection;
   }
 
